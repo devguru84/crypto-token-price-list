a01936da19bfc9d3d93f3b6a0ae06310c1637aca
FullStackGuru <162280013+devguru84@users.noreply.github.com>
2024-03-18 04:44:21 -0400

    Migrated to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,31 @@ import './App.css';
 import axios from 'axios'
 import Coin from './Coin';
 
+interface CoinData {
+  id: string;
+  name: string;
+  image: string;
+  symbol: string;
+  market_cap: number;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 function App() {
-  const [coins,setCoins] = useState([])
-  const [search,setSearch] = useState('')
+  const [coins,setCoins] = useState<CoinData[]>([])
+  const [search,setSearch] = useState<string>('')
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res=>{
+    .then(res => {
        setCoins(res.data)
        console.log(res.data)
-    }).catch(error=>console.log(error))
+    }).catch(error => console.log(error))
   }, [])
-  const handleChange = e =>{
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSearch(e.target.value)
   }
-  const filteredCoins = coins.filter(coin=>
+  const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLowerCase())
     )
   return (
@@ -30,8 +40,7 @@ function App() {
         </form>
 
       </div>
-      {filteredCoins.map(coin=>{
-        return(
+      {filteredCoins.map(coin => (
           <Coin 
           key={coin.id} 
           name={coin.name} 
@@ -42,10 +51,7 @@ function App() {
           pricechange={coin.price_change_percentage_24h}
 //           volume={coin.total_volume}
           />
-        );
-      })}
-
-
+      ))}
     </div>
   );
 }
--- src/Coin.tsx
+++ src/Coin.tsx
@@ -1,32 +1,40 @@
 import React from 'react'
 import './Coin.css'
 
-const Coin = ({image,name,price,volume,pricechange,marketcap}) => {
+interface CoinProps {
+    image: string;
+    symbol: string;
+    name: string;
+    price: number;
+    pricechange: number;
+    marketcap: number;
+}
+
+const Coin: React.FC<CoinProps> = ({ image, symbol, name, price, pricechange, marketcap }) => {
     return (
         <div className="coin-container">
             <div className="coin-row">
                 <div className="coin">
                     <img src={image} alt="crypto" />
                     <h1>{name}</h1>
-                    <p className="coin-symbol"></p>
+                    <p className="coin-symbol">{symbol}</p>
                 </div>
                 <div className="coin-data">
-                    <p className="coin-price">Rs.{price}</p>
-//                     <p className="coin-volume">Rs.{volume.toLocaleString()}</p>
-                    {pricechange<0 ? (
+                    <p className="coin-price">{price}</p>
+                    {pricechange < 0 ? (
                         <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
                     ):(
                         <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
                     )
                 }
                 <p className="coin-marketcap">
-                    Mkt Cap: Rs.{marketcap.toLocaleString()}
+                    Mkt Cap: {marketcap.toLocaleString()}
                 </p>
                 </div>
             </div>
             
         </div>
-    )
-}
+    );
+};
 
-export default Coin
+export default Coin;
